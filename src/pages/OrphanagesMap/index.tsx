31957d959cafe347580ee77b1650a0e3fe9bd8fb
Richard <rrobinson.campos@gmail.com>
2020-10-14 11:53:38 -0300

    Orphanages Map Page styles, Adding map api

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { PageMap, ButtonMap } from './styles';
+
+import { FiPlus } from 'react-icons/fi';
+
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../../images/map-marker.svg';
 
 const OrphanagesMap: React.FC = () => {
   return (
-    <Container>
-      <h1>OrphanagesMap</h1>
-    </Container>
+    <PageMap>
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita</p>
+        </header>
+
+        <footer>
+          <strong>São Paulo</strong>
+          <span>São Bernardo do Campo</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-23.6667916, -46.5644216]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+
+      <ButtonMap to="/">
+        <FiPlus size={32} color="#FFF" />
+      </ButtonMap>
+    </PageMap>
   );
 };
 
